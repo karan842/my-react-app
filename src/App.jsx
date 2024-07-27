d17ec2cf224db57594e9e1687bb5ee0742097e02
Karan Shingde <karanshingde@gmail.com>
2024-07-27 13:41:26 +0530

    ColorPicker App added using useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,6 @@
-// React hook: Special function that allows functional components
-                // to use React features without writing class components (React)
-                // (useState, useEffect, useContext, useReducer, useCallback, and more....)
-
-// useState() =  A React hook that allows the creation of a stateful variable
-                // AND a setter function to update its value in the virtual DOM. 
-                // [name, setName]
-import OnChange from './OnChange';
+import ColorPicker from "./ColorPicker";
 
 function App() {
-  return(<OnChange/>);
+  return(<ColorPicker/>);
 }
 export default App
